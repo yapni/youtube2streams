fa8ae820a47dc259f60c5ecd67477297fce22c80
yapni <yapni8@gmail.com>
2020-06-17 20:17:05 +0800

    Add logo and error msgs to popup

--- src/popup.js
+++ src/popup.js
@@ -4,12 +4,17 @@ import 'bootstrap/js/dist/button';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 chrome.storage.sync.get(["spotify_link", "applemusic_link", "deezer_link"], function(result) {
+    let error_msg_element = $("<div class='text-danger' style='font-size:11px'>Song not found</div>");
+
     // Spotify
     if (typeof result.spotify_link != "undefined") {
         $("#spotify_btn").attr("disabled", false);
         $("#spotify_btn").on("click", function() {
             window.open(result.spotify_link);
         });
+    }
+    else {
+        error_msg_element.clone().appendTo("#spotify_btn")
     }
 
     // Apple Music
@@ -19,6 +24,9 @@ chrome.storage.sync.get(["spotify_link", "applemusic_link", "deezer_link"], func
             window.open(result.applemusic_link);
         });
     }
+    else {
+        error_msg_element.clone().appendTo("#applemusic_btn")
+    }
 
     // Deezer
     if (typeof result.deezer_link != "undefined") {
@@ -26,5 +34,8 @@ chrome.storage.sync.get(["spotify_link", "applemusic_link", "deezer_link"], func
         $("#deezer_btn").on("click", function() {
             window.open(result.deezer_link);
         });
+    }
+    else {
+        error_msg_element.clone().appendTo("#deezer_btn")
     }
 });
