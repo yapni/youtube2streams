64b3d9c5b20a587f94a8f0a6a2edf0dc1c862260
yapni <yapni8@gmail.com>
2020-06-17 20:17:40 +0800

    Improve list of ending keywords

--- src/utils/ending.js
+++ src/utils/ending.js
@@ -1,29 +1,34 @@
 // List of strings at the end of a youtube video title
 var ending_strings = [
-    "M/V",
-    "MV",
+    // Surrounded by "[] or ()"
+    "[MV]",
     "[M/V]",
     "(M/V)",
     "[Music Video]",
-    "Music Video Official",
     "(Official Music Video)",
-    "Official Music Video",
-    "Official MV",
-    "Official M/V",
     "(Official Video)",
     "[Official Video]",
-    "Official Video",
-    "(Official Audio",
+    "(Official Audio)",
     "(Audio)",
-    "Audio",
-    "Lyrics",
     "(Lyrics)",
     "(Lyric Video)",
     "(Lyrics Video)",
+    "(Official Lyric Video)",
+    "(Official Lyrics Video)",
+    // Keywords on its own
     "Official Lyric Video",
     "Official Lyrics Video",
-    "(Official Lyric Video)",
-    "(Official Lyrics Video)"
+    "Official Video",
+    "Official M/V",
+    "Official MV",
+    "Official Music Video",
+    "Music Video Official",
+    // Can be part of a keyword/on its own
+    "Music Video",
+    "Audio",
+    "Lyrics",
+    "M/V",
+    "MV",
 ]
 
 export {ending_strings}
